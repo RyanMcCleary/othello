2b741dd0e7283713c8d025d910a4d65c3391e22f
Charlotte McCleary <charlotte.mccleary@knights.ucf.edu>
2023-03-18 08:47:54 -0400

    Changes made in every file and SquareIndex class added.

--- Game.java
+++ Game.java
@@ -1,13 +1,13 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
-
-	
-	Square[][] board;
-	Player currentPlayer;
-	int boardSize;
+	private Square[][] board;
+	private Player currentPlayer;
+	private int boardSize;
 	
 	
-	Game(int n) {
+	public Game(int n) {
 		this.currentPlayer = Player.WHITE;
 		this.board = new Square[n][n];
 		this.boardSize = n;
@@ -20,12 +20,44 @@ public class Game {
 		this.board[n / 2 - 1][n / 2] = this.board[n / 2][n / 2 - 1] = Square.BLACK;
 	}
 	
-	Game() {
+	public Game() {
 		this(8);
 	}
+	
+    void setSquare(int row, int col, Square s) {
+		this.board[row][col] = s;
+	}
+	
+	Square getSquare(int row, int col) {
+		return this.board[row][col];
+	}
 
+    boolean flipPiecesInDirection(int row, int col, int rowDelta, int colDelta) {
+		if (!isValidDirection(row, col, rowDelta, colDelta)) {
+			return false;
+		}
+		System.out.printf("flipPiecesInDirection(%d, %d, %d, %d)\n", row, col, rowDelta, colDelta);
+		row += rowDelta;
+		col += colDelta;
+		while (inBounds(row, col) && this.board[row][col] == oppositeColor()) {
+			setSquare(row, col, currentColor());
+			row += rowDelta;
+			col += colDelta;
+		}
+		return true;
+	}	
+	
+	Player switchPlayer() {
+		if (this.currentPlayer == Player.WHITE)
+			this.currentPlayer = Player.BLACK;
+		else {
+			this.currentPlayer = Player.WHITE;
+		}
+		return currentPlayer;
+	}
+	
 	// Prints board state
-	void printBoard() {
+	public void printBoard() {
 		for(int row = 0; row < this.boardSize; row++) {
 			for(int col =0; col < this.boardSize; col++) {
 				if(this.board[row][col] == Square.WHITE)
@@ -38,7 +70,7 @@ public class Game {
 		}
 	}
 	
-	Square currentColor() {
+	public Square currentColor() {
 		if (this.currentPlayer == Player.BLACK) {
 			return Square.BLACK;
 		}
@@ -47,7 +79,7 @@ public class Game {
 		}
 	}
 	
-	Square oppositeColor() {
+	public Square oppositeColor() {
 		if (this.currentPlayer == Player.BLACK) {
 			return Square.WHITE;
 		}
@@ -56,7 +88,7 @@ public class Game {
 		}
 	}
 	
-	boolean isValidDirection(int row, int col, int rowDelta, int colDelta) {
+	public boolean isValidDirection(int row, int col, int rowDelta, int colDelta) {
 		if (!inBounds(row, col) || !inBounds(row + rowDelta, col + colDelta)) {
 			return false;
 		}
@@ -77,7 +109,7 @@ public class Game {
 		}
 	}
 	
-	boolean isValidMove(int row, int col) {
+	public boolean isValidMove(int row, int col) {
 		for (int rowDelta = -1; rowDelta <= 1; rowDelta++) {
 			for (int colDelta = -1; colDelta <= 1; colDelta++) {
 				if (!(rowDelta == 0 && colDelta == 0) && isValidDirection(row, col, rowDelta, colDelta)) {
@@ -88,14 +120,50 @@ public class Game {
 		return false;
 	}
 	
-	boolean inBounds(int row, int col) {
+	public boolean inBounds(int row, int col) {
 		return (0 <= row && row < this.boardSize 
 				&& 0 <= col && col < this.boardSize);
 	}
-		
+    boolean makeMove(int row, int col) {
+		boolean validMove = false;
+		for (int rowDelta = -1; rowDelta <= 1; rowDelta++) {
+			for (int colDelta = -1; colDelta <= 1; colDelta++) {
+				if (!(rowDelta == 0 && colDelta == 0)) {
+					validMove = validMove || flipPiecesInDirection(row, col, rowDelta, colDelta);
+				}
+			}
+		}
+		if (validMove) {
+			setSquare(row, col, currentColor());
+		}
+		return validMove;
+	}	
+	
+	/**
+	 *  This function returns an ArrayList of Pair objects corresponding
+	 *  to all of the valid moves.
+	 */
+	public ArrayList<SquareIndex> movesList()  {
+		ArrayList<SquareIndex> moves = new ArrayList<>();	
+		for(int i = 0; i < boardSize; i++)  {
+			for(int j = 0; j < boardSize; j++)  {
+				if(isValidMove(i, j))
+					moves.add(new SquareIndex(i, j));
+			}
+			
+		}
+		return moves;
 	}
-
-
-
-
-
+	
+	/** 
+	 *  This function calls movesList to obtain a list of all possible moves.
+	 *  Then, we randomly pick moves from the array. We do this for both the computer and 
+	 *  the player until the game is over. The winning player is returned. 
+	 */
+	/*public takeTurn(Player player)  {
+		
+	}*/
+	/*public Player playRandomGame()  {
+	
+	}*/
+}
--- Main.java
+++ Main.java
@@ -3,7 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Game game = new Game();
+		
+		game.makeMove(4,2);
+		game.switchPlayer();
+		game.makeMove(5,4);
+		game.switchPlayer();
+		System.out.println(game.currentColor());
+		System.out.println(game.isValidDirection(4, 5, 0, -1));
+		//System.out.println(game.makeMove(4, 5));
 		game.printBoard();
+
 	}
 
 }
