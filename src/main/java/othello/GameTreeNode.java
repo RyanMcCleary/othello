f88da62ce8911e96c71315ad646d31d437de6d69
Ryan McCleary <remccleary@gmail.com>
2023-03-25 13:35:01 -0400

    Added constructor to GameTreeNode class

--- src/main/java/othello/GameTreeNode.java
+++ src/main/java/othello/GameTreeNode.java
@@ -2,8 +2,17 @@ package othello;
 import java.util.ArrayList;
 
 public class GameTreeNode {
-    private SquareIndex move;
+    
+    private GameState gameState;
     private ArrayList<GameTreeNode> children;
     private double empiricalReward;
     private int numVisits;
+    
+    public GameTreeNode(GameState gameState) {
+        this.gameState = gameState;
+        this.children = new ArrayList<GameTreeNode>();
+        this.empiricalReward = 0.0;
+        this.numVisits = 0;
+    }
+    
 }
