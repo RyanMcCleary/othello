47ff653c4f626e6182742a727030a06722dee5e1
Ryan McCleary <remccleary@gmail.com>
2023-03-29 17:37:27 -0400

    Added constructor to GameTreeNode paking parent; added isRoot method to GameTreeNode

--- src/main/java/othello/GameTreeNode.java
+++ src/main/java/othello/GameTreeNode.java
@@ -5,14 +5,24 @@ public class GameTreeNode {
     
     private GameState gameState;
     private ArrayList<GameTreeNode> children;
+    private GameTreeNode parent;
     private double empiricalReward;
     private int numVisits;
     
-    public GameTreeNode(GameState gameState) {
+    public GameTreeNode(GameState gameState, GameTreeNode parent) {
         this.gameState = gameState;
         this.children = new ArrayList<GameTreeNode>();
+        this.parent = parent;
         this.empiricalReward = 0.0;
         this.numVisits = 0;
+    }
+    
+    public GameTreeNode(GameState gameState) {
+        this(gameState, null);
+    }
+    
+    public boolean isRoot() {
+        return this.parent == null;
     }
     
 }
