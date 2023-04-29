14de4f028399433d089a7252146a154e1c05d2ff
Ryan McCleary <remccleary@gmail.com>
2023-04-29 00:05:51 -0400

    Added very basic frontend web UI

---
+++ web/othello.js
@@ -0,0 +1,36 @@
+const boardSize = 8;
+const board = document.getElementById('board');
+
+for (let i = 0; i < boardSize; i++) {
+    const row = board.insertRow();
+    for (let j = 0; j < boardSize; j++) {
+        const cell = row.insertCell();
+        if ((i === 3 && j === 3) || (i === 4 && j === 4)) {
+            cell.classList.add('occupied', 'white');
+            cell.style.backgroundColor = 'green';
+        }
+        else if ((i === 3 && j === 4) || (i === 4 && j === 3)) {
+            cell.classList.add('occupied', 'black');
+            cell.style.backgroundColor = 'green';
+        }
+        else {
+            cell.onclick = () => play(cell);
+        }
+    }
+}
+
+let currentPlayer = 'black';
+
+function play(cell) {
+    if (cell.classList.contains('black') || cell.classList.contains('white')) {
+        return;
+    }
+    cell.classList.add('occupied', currentPlayer);
+    cell.style.backgroundColor = 'green';
+    if (currentPlayer === 'black') {
+        currentPlayer = 'white';
+    }
+    else {
+        currentPlayer = 'black';
+    }
+}
