c95fe7aa611365fc78b56f380e6ecbfdf48a985f
Ryan McCleary <remccleary@gmail.com>
2023-05-02 12:52:36 -0400

    Added test for dynamically allocated game_state

--- native/game_state_test.c
+++ native/game_state_test.c
@@ -1,8 +1,21 @@
+#include <stdio.h>
+#include <errno.h>
 #include "game_state.h"
 
 int main(int argc, char **argv) {
-    struct game_state state;
+    struct game_state state, *state_p;
+    puts("Testing statically allocated game_state...");
     game_state_init(&state);
     game_state_print(&state);
+
+    puts("\nTesting dynamically allocated game_state...");
+    if ((state_p = game_state_alloc(1)) == NULL) {
+        perror("game_state_alloc returned NULL");
+        return 1;
+    }
+    game_state_init(state_p);
+    game_state_print(state_p);
+    game_state_free(state_p);
+
     return 0;
 }
