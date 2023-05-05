3828c10b8b29e1da8891ec0a4ed28fe39c352b8c
Charlotte McCleary <charlotte.mccleary@knights.ucf.edu>
2023-05-05 17:27:04 -0400

    functions added to game_state.c

--- native/game_state.c
+++ native/game_state.c
@@ -17,7 +17,49 @@ struct game_state *game_state_init(struct game_state *state) {
 void game_state_free(struct game_state *state) {
     free(state);
 }
+bool isValidDirection(struct game_state *state, int row, int col, int rowDelta, int colDelta) {
+		if (!in_bounds(row, col) || !in_bounds(row + rowDelta, col + colDelta)) {
+			return false;
+		}
+		if (state->board[row + rowDelta][col + colDelta] != opposite_color(state)) {
+			return false;
+		}
+		row += rowDelta;
+		col += colDelta;
+		while (in_bounds(row, col) && state->board[row][col] == opposite_color(state)) {
+			row += rowDelta;
+			col += colDelta;
+		}
+		if (in_bounds(row, col) && state->board[row][col] == current_color()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+}
 
+bool in_bounds(int row, int col) {
+		return (0 <= row && row < 8 && 0 <= col && col < 8);
+	}
+
+
+enum square_state current_color(struct game_state *state) {	
+	if(state->player = player_black) {
+		return square_black;
+	}
+	else {
+		return square_white;
+	}	
+}
+
+enum square_state opposite_color(struct game_state *state) {	
+	if(state->player = player_black) {
+		return square_white;
+	}
+	else {
+		return square_black;
+	}
+}
 void game_state_print(struct game_state *state) {
     if (state->current_player == PLAYER_BLACK) {
         puts("current_player: PLAYER_BLACK");
@@ -46,4 +88,19 @@ void game_state_print(struct game_state *state) {
         }
         putchar('\n');
     }
+}
+
+bool game_state_flip_direction(struct game_state *state, int row, int col, int rowDelta, int colDelta) 
+{
+		if (!isValidDirection(row, col, rowDelta, colDelta)) {
+			return false;
+		}
+		row += rowDelta;
+		col += colDelta;
+		while (isInBounds(row, col) && this.board[row][col] == getOppositeColor()) {
+			setSquare(row, col, getCurrentColor());
+			row += rowDelta;
+			col += colDelta;
+		}
+		return true;
 }
