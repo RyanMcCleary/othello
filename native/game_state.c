67f6adf8c29780aa022e2e2e3a6537677a1a2468
Ryan McCleary <remccleary@gmail.com>
2023-05-05 18:09:38 -0400

    Fixed some errors

--- native/game_state.c
+++ native/game_state.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "game_state.h"
+
+
+static bool in_bounds(int row, int col);
+
+static enum square_state opposite_color(struct game_state *state);
+
+static enum square_state current_color(struct game_state *state);
 
 struct game_state *game_state_alloc(size_t num_elements) {
     return malloc(num_elements * sizeof(struct game_state));
@@ -17,49 +25,54 @@ struct game_state *game_state_init(struct game_state *state) {
 void game_state_free(struct game_state *state) {
     free(state);
 }
-bool isValidDirection(struct game_state *state, int row, int col, int rowDelta, int colDelta) {
-		if (!in_bounds(row, col) || !in_bounds(row + rowDelta, col + colDelta)) {
-			return false;
-		}
-		if (state->board[row + rowDelta][col + colDelta] != opposite_color(state)) {
-			return false;
-		}
-		row += rowDelta;
-		col += colDelta;
-		while (in_bounds(row, col) && state->board[row][col] == opposite_color(state)) {
-			row += rowDelta;
-			col += colDelta;
-		}
-		if (in_bounds(row, col) && state->board[row][col] == current_color()) {
-			return true;
-		}
-		else {
-			return false;
-		}
+
+static bool valid_direction(struct game_state *state, int row, int col,
+                      int row_delta, int col_delta) {
+    if (!in_bounds(row, col) ||
+        !in_bounds(row + row_delta, col + col_delta)) {
+        return false;
+    }
+    if (state->board[row + row_delta][col + col_delta] != opposite_color(state)) {
+        return false;
+    }
+    row += row_delta;
+    col += col_delta;
+    while (in_bounds(row, col) &&
+           state->board[row][col] == opposite_color(state)) {
+        row += row_delta;
+        col += col_delta;
+    }
+    if (in_bounds(row, col) &&
+        state->board[row][col] == current_color(state)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-bool in_bounds(int row, int col) {
-		return (0 <= row && row < 8 && 0 <= col && col < 8);
-	}
-
-
-enum square_state current_color(struct game_state *state) {	
-	if(state->player = player_black) {
-		return square_black;
-	}
-	else {
-		return square_white;
-	}	
+static bool in_bounds(int row, int col) {
+    return (0 <= row && row < 8 && 0 <= col && col < 8);
 }
 
-enum square_state opposite_color(struct game_state *state) {	
-	if(state->player = player_black) {
-		return square_white;
-	}
-	else {
-		return square_black;
-	}
+static enum square_state current_color(struct game_state *state) {	
+    if(state->current_player == PLAYER_BLACK) {
+        return SQUARE_BLACK;
+    }
+    else {
+        return SQUARE_WHITE;
+    }
 }
+
+static enum square_state opposite_color(struct game_state *state) {	
+    if(state->current_player == PLAYER_BLACK) {
+        return SQUARE_WHITE;
+    }
+    else {
+        return SQUARE_BLACK;
+    }
+}
+
 void game_state_print(struct game_state *state) {
     if (state->current_player == PLAYER_BLACK) {
         puts("current_player: PLAYER_BLACK");
@@ -90,17 +103,17 @@ void game_state_print(struct game_state *state) {
     }
 }
 
-bool game_state_flip_direction(struct game_state *state, int row, int col, int rowDelta, int colDelta) 
-{
-		if (!isValidDirection(row, col, rowDelta, colDelta)) {
-			return false;
-		}
-		row += rowDelta;
-		col += colDelta;
-		while (isInBounds(row, col) && this.board[row][col] == getOppositeColor()) {
-			setSquare(row, col, getCurrentColor());
-			row += rowDelta;
-			col += colDelta;
-		}
-		return true;
+static bool flip_direction(struct game_state *state, int row, int col, int row_delta, int col_delta) {
+    if (!valid_direction(state, row, col, row_delta, col_delta)) {
+        return false;
+    }
+    row += row_delta;
+    col += col_delta;
+    while (in_bounds(row, col) &&
+           state->board[row][col] == opposite_color(state)) {
+        state->board[row][col] = current_color(state);
+        row += row_delta;
+        col += col_delta;
+    }
+    return true;
 }
