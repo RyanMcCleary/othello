186deda0113ede4ddc94ab54b7e9e4fa7eeea33b
Ryan McCleary <remccleary@gmail.com>
2023-05-19 16:16:23 -0400

    s/pool/node_pool/g

--- native/pool_example.c
+++ native/pool_example.c
@@ -6,11 +6,11 @@ vint main(int argc, char **argv)
 {
     struct pool node_pool;
     pool_init(&node_pool, 1024 * 1024);
-    game_tree_node *nodes = pool_alloc(&pool, 10);
+    game_tree_node *nodes = pool_alloc(&node_pool, 10);
     for (size_t i = 0; i < 10; i++) {
         game_state_init(&nodes[i]->game_state);
         game_state_print(&nodes[i]->game_state);
     }
-    pool_destroy(&pool);
+    pool_destroy(&node_pool);
     return 0;
 }
