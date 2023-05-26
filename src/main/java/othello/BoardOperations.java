16e3cfaf4076b318caf5c764ef63fec8872f9487
unknown <justinrc10@gmail.com>
2023-05-26 18:26:48 -0400

    intermediate commit

--- src/main/java/othello/BoardOperations.java
+++ src/main/java/othello/BoardOperations.java
@@ -2,72 +2,160 @@ package othello;
 
 public class BoardOperations {
 
+    public static Square getSquare(long bboardB, long bboardW, int row, int col) {
+        Square square = Square.EMPTY;
+        long position = indexToLong(row, col);
+        if ((bboardB & position) != 0) {
+            square = Square.BLACK;
+        }
+        else if ((bboardW & position) != 0) {
+            square = Square.WHITE;
+        }
+        return square;
+    }
+
     public static long setSquare(long bboardB, long bboardW, int row, int col, Square square) {
         long position = indexToLong(row, col);
+        long newBoard = 0;
         if (square == Square.BLACK) {
-            bboardB |= position;
-            return bboardB;
+            newBoard = bboardB | position;
         }
         else if (square == Square.WHITE) {
-            bboardW |= position;
-            return bboardW;
+            newBoard = bboardW | position;
         }
         else {
             if (getSquare(bboardB, bboardW, row, col) == Square.BLACK) {
-                bboardB = ~((~bboardB) | position);
-                return bboardB;
+                newBoard = ~((~bboardB) | position);
             }
             else if (getSquare(bboardB, bboardW, row, col) == Square.WHITE) {
-                bboardW = ~((~bboardW) | position);
-                return bboardW;
+                newBoard = ~((~bboardW) | position);
             }
         }
-        return 0;
+        return newBoard;
     }
 
-    public static long setSquare(long bboard, int row, int col) {
-        long position = indexToLong(row, col);
-        bboard |= position;
-        return bboard;
-    }
-
-    public static Square getSquare(long bboardB, long bboardW, int row, int col) {
-        long position = indexToLong(row, col);
-        if ((bboardB & position) != 0) {
-            return Square.BLACK;
-        }
-        else if ((bboardW & position) != 0) {
-            return Square.WHITE;
-        }
-        else {
-            return Square.EMPTY;
-        }
+    private static long setSquare(long bboard, long position) {
+        return bboard | position;
     }
 
     private static long indexToLong(int row, int col) {
-        long position = 1L << ((8 * (col - 1)) + (row - 1));
-        return position;
+        return 1L << ((8 * (col - 1)) + (row - 1));
     }
 
-    private static boolean isLegal(long bboardB, long bboardW, int row, int col, Player player) {
-        return false;
-    }
-    
-    public static long getLegalMoves(long bboardB, long bboardW, Player player) {
-        long legalMoves = 0;
-        for (int row = 1; row <= 8; row++) {
-            for (int col = 1; col <= 8; col++) {
-                if (isLegal(bboardB, bboardW, row, col, player)) {
-                    if (player == Player.BLACK) {
-                        legalMoves = setSquare(legalMoves, bboardW, row, col, Square.BLACK);
-                    }
-                    else {
-                        legalMoves = setSquare(bboardB, legalMoves, row, col, Square.WHITE);
-                    }
+    public static Square[][] longsToBoard(long bboardB, long bboardW) {
+        Square[][] board = new Square[8][8];
+        for (int i=1; i<=8; i++) {
+            for (int j=1; j<=8; j++) {
+                long position = indexToLong(i,j);
+                if ((bboardB & position) != 0) {
+                    board[i][j] = Square.BLACK;
+                }
+                else if ((bboardW & position) != 0) {
+                    board[i][j] = Square.WHITE;
+                }
+                else {
+                    board[i][j] = Square.EMPTY;
                 }
             }
         }
+        return board;
+    }
+    
+    // bboardP is the current player's bitboard and bboardO is the opponent's bitboard
+    public static long getLegalMoves(long bboardP, long bboardO) {
+        long legalMoves = 0;
+        long position = 1;
+        for (int i = 1; i < 64; i++) {
+            if (isLegal(bboardP, bboardO, position)) {
+                    legalMoves = setSquare(legalMoves, position);
+                }
+            position = position << 1;
+            }
         return legalMoves;
+    }
+
+    private static boolean isLegal(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        if (isVacant(bboardP, bboardO, position)) {
+            if (checkE(bboardP, bboardO, position) | checkNE(bboardP, bboardO, position) | checkN(bboardP, bboardO, position) | checkNW(bboardP, bboardO, position) | checkW(bboardP, bboardO, position) | checkSW(bboardP, bboardO, position) | checkS(bboardP, bboardO, position) | checkSE(bboardP, bboardO, position)) {
+                legal = true;
+            }
+        }
+        return legal;
+    }
+    
+    private static boolean isVacant(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        if ((position & (bboardP | bboardO)) == 0) {
+            legal = true;
+        }
+        return legal;
+    }
+    
+    private static boolean checkE(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        long EShift = position >> 1;
+        long twoRightMostCols = 0; // fix
+        long sevenLeftMostCols = 0; // fix
+        if ((position & twoRightMostCols) == 0) {
+            while (((EShift & bboardO) != 0) & ((EShift & sevenLeftMostCols) != 0)) {
+                if (((EShift >> 1) & bboardP) != 0) {
+                    legal = true;
+                }
+            }
+        }
+        return legal;
+    }
+
+    private static boolean checkNE(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        // fix
+        return legal;
+    }
+
+    private static boolean checkN(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        long NShift = position << 8;
+        long twoRightMostCols = 0; // fix
+        long sevenLeftMostCols = 0; // fix
+        if ((position & twoRightMostCols) == 0) {
+            while (((NShift & bboardO) != 0) & ((NShift & sevenLeftMostCols) != 0)) {
+                if (((NShift >> 1) & bboardP) != 0) {
+                    legal = true;
+                }
+            }
+        }
+        return legal;
+    }
+
+    private static boolean checkNW(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        // fix
+        return legal;
+    }
+
+    private static boolean checkW(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        // fix
+        return legal;
+    }
+
+    private static boolean checkSW(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        // fix
+        return legal;
+    }
+
+    private static boolean checkS(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        // fix
+        return legal;
+    }
+
+    private static boolean checkSE(long bboardP, long bboardO, long position) {
+        boolean legal = false;
+        // fix
+        return legal;
     }
 
 }
