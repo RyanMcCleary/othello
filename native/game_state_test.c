adcf8db60f0357975ce4ecc24f3acca7534c9694
Ryan McCleary <remccleary@gmail.com>
2023-06-21 16:35:15 -0400

    added tests

--- native/game_state_test.c
+++ native/game_state_test.c
@@ -2,31 +2,37 @@
 #include <errno.h>
 #include "game_state.h"
 
-int main(int argc, char **argv) {
-    struct game_state state, *state_p;
-    puts("Testing statically allocated game_state...");
-    game_state_init(&state);
-    game_state_print(&state);
-
-    puts("\nTesting dynamically allocated game_state...");
-    if ((state_p = game_state_alloc(1)) == NULL) {
-        perror("game_state_alloc returned NULL");
+int othello_assert(bool condition, const char *msg) {
+    if (!condition) {
+        fprintf(stderr, "%s test FAILED. :(\n", msg);
+        return 0;
+    } else {
+        printf("%s test PASSED! :)\n", msg);
         return 1;
     }
-    game_state_init(state_p);
-    game_state_print(state_p);
-    game_state_free(state_p);
+}
 
-    while (true) {
-        int row = 0, col = 0;
-        printf("Enter row and column for move: ");
-        fflush(stdout);
-        scanf("%d %d", &row, &col);
-        if (game_state_valid_move(&state, row, col)) {
-            game_state_make_move(&state, row, col);
-            game_state_print(&state);
-        }
-    }
+int test_alloc(void) {
+    struct game_state *state = game_state_alloc(1);
+    int passed = othello_assert(state != NULL, "game_state_alloc(1)");
+    if (state != NULL) game_state_free(state);
+    return passed;
+}
 
+int test_init(void) {
+    struct game_state state;
+    game_state_init(&state);
+    int passed = othello_assert(state.board[3][3] == SQUARE_BLACK &&
+        state.board[4][4] == SQUARE_BLACK && state.board[3][4] == SQUARE_WHITE &&
+        state.board[4][3] == SQUARE_WHITE, "game_state_init center center squares");
+    passed += othello_assert(state.current_player == PLAYER_BLACK, "game_state_init current player");
+    return passed;
+}
+
+int main(int argc, char **argv) {
+    test_alloc();
+    test_init();
+    
     return 0;
 }
+
