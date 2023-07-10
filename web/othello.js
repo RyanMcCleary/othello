31623259a72d07d58e527985e3a79d7ae63179c1
Charlotte McCleary <charlotte.mccleary@knights.ucf.edu>
2023-07-09 21:00:50 -0400

    added player tracker on web

--- web/othello.js
+++ web/othello.js
@@ -5,13 +5,16 @@ for (let i = 0; i < boardSize; i++) {
     const row = board.insertRow();
     for (let j = 0; j < boardSize; j++) {
         const cell = row.insertCell();
+        cell.setAttribute("tabIndex", "0");
         if ((i === 3 && j === 3) || (i === 4 && j === 4)) {
             cell.classList.add('occupied', 'white');
             cell.style.backgroundColor = 'green';
+            cell.ariaLabel = "white";
         }
         else if ((i === 3 && j === 4) || (i === 4 && j === 3)) {
             cell.classList.add('occupied', 'black');
             cell.style.backgroundColor = 'green';
+            cell.ariaLabel = "black";
         }
         else {
             cell.onclick = () => play(cell);
@@ -20,6 +23,7 @@ for (let i = 0; i < boardSize; i++) {
 }
 
 let currentPlayer = 'black';
+document.getElementById("currentplayer").innerHTML = "Black to Go";
 
 function play(cell) {
     if (cell.classList.contains('black') || cell.classList.contains('white')) {
@@ -29,8 +33,10 @@ function play(cell) {
     cell.style.backgroundColor = 'green';
     if (currentPlayer === 'black') {
         currentPlayer = 'white';
+        document.getElementById("currentplayer").innerHTML = "White to Go";
     }
     else {
         currentPlayer = 'black';
+        document.getElementById("currentplayer").innerHTML = "Black to Go";
     }
 }
